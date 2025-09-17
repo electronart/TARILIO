9428db7718185d1313103c601e13c5c5aff8f64b
Thomas Harris <thomas.harris@electronart.co.uk>
2025-09-17 10:42:57 +0100

    Exclude .class files

--- eSearch/Models/Documents/Parse/TikaParser3.cs
+++ eSearch/Models/Documents/Parse/TikaParser3.cs
@@ -22,7 +22,7 @@ namespace eSearch.Models.Documents.Parse
             get { 
                 return new string[] { 
                         // application/java-vm
-                    "class",
+                    // "class",
     
                     // audio/x-wav
                     "wav",
