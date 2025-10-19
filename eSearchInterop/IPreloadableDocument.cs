b68cd95ead15edc5bcb63287662023847b703e50
Thomas Harris <thomas.harris@electronart.co.uk>
2025-10-19 14:19:07 +0100

    Indexing Performance - Introduce IPreloadableDocument

---
+++ eSearchInterop/IPreloadableDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSearch.Interop
+{
+    public interface IPreloadableDocument : IDocument
+    {
+        /// <summary>
+        /// The thread blocking preload task.
+        /// Preload document fields etc.
+        /// eSearch will call this on preloadable documents when indexing
+        /// Allowing eSearch to be preloading more than one document at a time.
+        /// </summary>
+        public Task PreloadDocument();
+    }
+}
